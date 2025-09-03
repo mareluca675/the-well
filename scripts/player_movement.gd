829205ace1f57ddc89735194a637943546d7e839
Max <maxsamov@gmail.com>
2025-09-03 22:13:25 +0300

    Add crouch and sprint
    Added basic crouching and sprinting actions, removed boilerplate comments

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -3,10 +3,11 @@ extends CharacterBody3D
 @onready var camera_3d = $Camera3D
 
 const SPEED = 100.0
+const CROUCH_MOD = 0.5
+const SPRINT_MOD = 2
 const JUMP_VELOCITY = 50
 const CAMERA_SENS = 0.003
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 100
+const gravity = 100
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -20,23 +21,32 @@ func _input(event):
 		rotation.x = clamp(rotation.x, -0.5, 1.2)
 
 func _physics_process(delta):
-	# Add the gravity.
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	
+	var crouching = Input.is_action_pressed("crouch")
+	var sprinting = Input.is_action_pressed("run")
+	var modifier
+	
+	if crouching:
+		modifier = CROUCH_MOD
+	elif sprinting:
+		modifier = SPRINT_MOD
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		modifier = 1
+	
+	if direction:
+		velocity.x = direction.x * SPEED * modifier
+		velocity.z = direction.z * SPEED * modifier
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED * modifier)
+		velocity.z = move_toward(velocity.z, 0, SPEED * modifier)
 
 	move_and_slide()
